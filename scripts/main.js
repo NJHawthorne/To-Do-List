99d1d8abd4132cbceb02a15ebab7d3061c19b65a
Nate Hawthorne <natejhawthorne@gmail.com>
2016-03-18 14:10:28 -0500

    Initial commit with basic layout.

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+var submitList = document.querySelector('.submitListBtn');
+var toDoEntry = document.querySelector('.listItemBar');
+submitList.addEventListener('click', addEntry);
+
+function addEntry () {
+	
+}
