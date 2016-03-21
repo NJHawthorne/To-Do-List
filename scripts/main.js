b041827f3bc604ff11d919a72e92add04de31e64
Nate Hawthorne <natejhawthorne@gmail.com>
2016-03-20 23:39:06 -0500

    Added the main explorer functions and event listeners. The important thing is the list works.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,34 @@
+//Establishing query selectors for later
 var submitList = document.querySelector('.submitListBtn');
 var toDoEntry = document.querySelector('.listItemBar');
-submitList.addEventListener('click', addEntry);
+var userEntry = document.querySelector('.userEntry');
+var listEntries = document.querySelector('.listEntries');
 
-function addEntry () {
-	
+//setting the user entry to an empty array so I can store entries. From the user. Yee.
+var userEntry = [];
+
+//Setting up a function I can call later
+function renderEntries() {
+	//Whatever the new input is, I'm creating this variable for it and making it blank
+	var newEntries = '';
+	//Setting up a for loop so that every time the user clicks, these things happen
+	for (var i = 0; i < userEntry.length; i++) {
+		//Setting new entries equal to whatever the user inputs and wrapping it in a div
+		newEntries += '<p>'+userEntry[i]+'</p>\n\n';
+		//Writing everything in the div towards the bottom. You know, beneath the obnoxious bits of text. 
+		listEntries.innerHTML = newEntries;
+	}
 }
+
+//Setting up a simple function to clear the input bar whenever it's clicked. 
+toDoEntry.addEventListener('click', function() {
+	toDoEntry.value = '';
+});
+
+//Last little event listener to make the button actually do what it's supposed to do. Kinda important.
+submitList.addEventListener('click', function() {
+	//Pushing whatever the user inputs into the array
+	userEntry.push(toDoEntry.value);
+	//Calling the function that does all the work. ALL THE DAMN WORK.
+	renderEntries();
+});
